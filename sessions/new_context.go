8d3cc339633aad5cabd74523ba930ae17a2cdc4c
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2024-12-12 12:26:09 -0700

    fix comments

--- sessions/new_context.go
+++ sessions/new_context.go
@@ -9,12 +9,12 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// ContextWithToken returns a copy of ctx that stores the Token
+// NewContextWithToken returns a copy of ctx that stores the Token
 func NewContextWithToken(ctx context.Context, token *oauth2.Token) context.Context {
 	return contextx.With(ctx, token)
 }
 
-// OhAuthTokenFromContext returns the Token from the ctx
+// NewOhAuthTokenFromContext returns the Token from the ctx
 func NewOhAuthTokenFromContext(ctx context.Context) (*oauth2.Token, error) {
 	token, ok := contextx.From[*oauth2.Token](ctx)
 	if !ok {
@@ -24,7 +24,7 @@ func NewOhAuthTokenFromContext(ctx context.Context) (*oauth2.Token, error) {
 	return token, nil
 }
 
-// UserIDFromContext returns the user ID from the ctx
+// NewUserIDFromContext returns the user ID from the ctx
 // this function assumes the session data is stored in a string map
 func NewUserIDFromContext(ctx context.Context) (string, error) {
 	sessionDetails, ok := contextx.From[*Session[any]](ctx)
@@ -54,7 +54,7 @@ func NewUserIDFromContext(ctx context.Context) (string, error) {
 
 type UserID string
 
-// ContextWithUserID returns a copy of ctx that stores the user ID
+// NewContextWithUserID returns a copy of ctx that stores the user ID
 func NewContextWithUserID(ctx context.Context, userID UserID) context.Context {
 	if userID == "" {
 		return ctx
@@ -63,7 +63,7 @@ func NewContextWithUserID(ctx context.Context, userID UserID) context.Context {
 	return contextx.With(ctx, userID)
 }
 
-// SessionToken returns the session token from the context
+// NewSessionToken returns the session token from the context
 func NewSessionToken(ctx context.Context) (string, error) {
 	sd, err := newGetSessionDataFromContext(ctx)
 	if err != nil {
@@ -76,12 +76,12 @@ func NewSessionToken(ctx context.Context) (string, error) {
 	return sd.store.EncodeCookie(sd)
 }
 
-// addSessionDataToContext adds session data to the context
+// NewAddSessionDataToContext adds session data to the context
 func (s *Session[P]) NewAddSessionDataToContext(ctx context.Context) context.Context {
 	return contextx.With(ctx, s)
 }
 
-// getSessionDataFromContext retrieves session data from the context
+// newGetSessionDataFromContext retrieves session data from the context
 func newGetSessionDataFromContext(ctx context.Context) (*Session[map[string]any], error) {
 	sessionData, ok := contextx.From[*Session[map[string]any]](ctx)
 	if !ok {
