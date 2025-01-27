31fa9e3d6ad0612a166176fbdb056b0e44bf42be
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-01-27 09:06:13 -0700

    feat: align defaults with config default
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- totp/totp.go
+++ totp/totp.go
@@ -384,8 +384,8 @@ func GenerateOTP(secret string) (string, error) {
 func (o *OTP) GenerateRecoveryCodes() []string {
 	codes := []string{}
 
-	// for range o.recoveryCodeCount { // this works in go 1.22 but the linter barfs while its still on 1.21
-	for i := 1; i <= o.recoveryCodeCount; i++ {
+	for range o.recoveryCodeCount {
+		log.Debug().Int("length", o.recoveryCodeLength).Msg("generating recovery code")
 		code, err := String(o.recoveryCodeLength, alphanumericCode)
 		if err != nil {
 			continue
