bf365d13d7778b0a6afedb6f29e5c56eb88d8869
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-02-11 21:54:53 -0700

    bump github version
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- providers/github/login.go
+++ providers/github/login.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/google/go-github/v68/github"
+	"github.com/google/go-github/v69/github"
 	"golang.org/x/oauth2"
 
 	oauth2Login "github.com/theopenlane/iam/providers/oauth2"
