d4bae601c908fdb006b1b01f52ab3d84752d49a0
Achiket123 <achiket.kumar@onlyoncloud.com>
2025-10-19 00:27:04 +0530

    Revert "Changes for add RequestOptions and support X-Request-Id per-request headers"
    This reverts commit 64997ef06a0e0895d0a2a325e61ad92c460e8c35.

--- fgax/options.go
+++ fgax/options.go
@@ -1,19 +1,19 @@
 package fgax
 
 import (
-	openfga "github.com/openfga/go-sdk"
-	ofgaclient "github.com/openfga/go-sdk/client"
+    openfga "github.com/openfga/go-sdk"
+    ofgaclient "github.com/openfga/go-sdk/client"
 )
 
 const (
-	// HeaderXRequestID is the header name for request id
-	HeaderXRequestID = "X-Request-Id"
+    // HeaderXRequestID is the header name for request id
+    HeaderXRequestID = "X-Request-Id"
 )
 
 // RequestOptions holds per-request options that can be applied to OpenFGA requests
 type RequestOptions struct {
-	Headers     map[string]string
-	Consistency *openfga.ConsistencyPreference
+    Headers     map[string]string
+    Consistency *openfga.ConsistencyPreference
 }
 
 // RequestOption is a functional option for RequestOptions
@@ -21,105 +21,111 @@ type RequestOption func(*RequestOptions)
 
 // WithRequestIDHeader sets the X-Request-Id header for the request
 func WithRequestIDHeader(id string) RequestOption {
-	return func(ro *RequestOptions) {
-		if ro.Headers == nil {
-			ro.Headers = map[string]string{}
-		}
+    return func(ro *RequestOptions) {
+        if ro.Headers == nil {
+            ro.Headers = map[string]string{}
+        }
 
-		ro.Headers[HeaderXRequestID] = id
-	}
+        ro.Headers[HeaderXRequestID] = id
+    }
 }
 
 // WithCustomHeader sets a custom header for the request
 func WithCustomHeader(header, value string) RequestOption {
-	return func(ro *RequestOptions) {
-		if ro.Headers == nil {
-			ro.Headers = map[string]string{}
-		}
+    return func(ro *RequestOptions) {
+        if ro.Headers == nil {
+            ro.Headers = map[string]string{}
+        }
 
-		ro.Headers[header] = value
-	}
+        ro.Headers[header] = value
+    }
 }
 
 // WithHighConsistency sets the consistency preference to higher consistency
 func WithHighConsistency() RequestOption {
-	return func(ro *RequestOptions) {
-		c := openfga.CONSISTENCYPREFERENCE_HIGHER_CONSISTENCY
-		ro.Consistency = &c
-	}
+    return func(ro *RequestOptions) {
+        c := openfga.CONSISTENCYPREFERENCE_HIGHER_CONSISTENCY
+        ro.Consistency = &c
+    }
 }
 
 // getRequestOptions aggregates functional RequestOptions into a RequestOptions struct
 func getRequestOptions(opts ...RequestOption) RequestOptions {
-	ro := RequestOptions{
-		Consistency: &defaultConsistency,
-	}
+    ro := RequestOptions{}
 
-	for _, o := range opts {
-		o(&ro)
-	}
+    for _, o := range opts {
+        o(&ro)
+    }
 
-	return ro
+    return ro
 }
 
 // convert to ofgaclient.ClientCheckOptions
 func getCheckOptions(opts ...RequestOption) ofgaclient.ClientCheckOptions {
-	ro := getRequestOptions(opts...)
+    ro := getRequestOptions(opts...)
 
-	o := ofgaclient.ClientCheckOptions{}
+    o := ofgaclient.ClientCheckOptions{}
 
-	o.Consistency = ro.Consistency
+    if ro.Consistency != nil {
+        o.Consistency = ro.Consistency
+    }
 
-	if len(ro.Headers) > 0 {
-		o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
-	}
+    if len(ro.Headers) > 0 {
+        o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
+    }
 
-	return o
+    return o
 }
 
 func getBatchCheckOptions(opts ...RequestOption) ofgaclient.BatchCheckOptions {
-	ro := getRequestOptions(opts...)
+    ro := getRequestOptions(opts...)
 
-	o := ofgaclient.BatchCheckOptions{
-		MaxBatchSize:        &batchSizeLimit,
-		MaxParallelRequests: &batchParallelLimit,
-	}
+    o := ofgaclient.BatchCheckOptions{
+        Consistency:         &defaultConsistency,
+        MaxBatchSize:        &batchSizeLimit,
+        MaxParallelRequests: &batchParallelLimit,
+    }
 
-	o.Consistency = ro.Consistency
+    if ro.Consistency != nil {
+        o.Consistency = ro.Consistency
+    }
 
-	if len(ro.Headers) > 0 {
-		o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
-	}
+    if len(ro.Headers) > 0 {
+        o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
+    }
 
-	return o
+    return o
 }
 
 func getReadOptions(opts ...RequestOption) ofgaclient.ClientReadOptions {
-	ro := getRequestOptions(opts...)
+    ro := getRequestOptions(opts...)
 
-	o := ofgaclient.ClientReadOptions{
-		PageSize: openfga.PtrInt32(defaultPageSize),
-	}
+    o := ofgaclient.ClientReadOptions{
+        PageSize:    openfga.PtrInt32(defaultPageSize),
+        Consistency: &defaultConsistency,
+    }
 
-	o.Consistency = ro.Consistency
+    if ro.Consistency != nil {
+        o.Consistency = ro.Consistency
+    }
 
-	if len(ro.Headers) > 0 {
-		o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
-	}
+    if len(ro.Headers) > 0 {
+        o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
+    }
 
-	return o
+    return o
 }
 
 func getWriteOptions(opts ...RequestOption) ofgaclient.ClientWriteOptions {
-	ro := getRequestOptions(opts...)
+    ro := getRequestOptions(opts...)
 
-	o := ofgaclient.ClientWriteOptions{}
+    o := ofgaclient.ClientWriteOptions{}
 
-	if len(ro.Headers) > 0 {
-		o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
-	}
+    if len(ro.Headers) > 0 {
+        o.RequestOptions = ofgaclient.RequestOptions{Headers: ro.Headers}
+    }
 
-	// write options do not support a consistency field in the SDK
+    // write options do not support a consistency field in the SDK
 
-	return o
+    return o
 }
