21d3f50d3e264ca3e10b86545ca29df6d64cbbd0
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-03-01 08:32:33 -0600

    last context key update

--- providers/google/context.go
+++ providers/google/context.go
@@ -3,25 +3,23 @@ package google
 import (
 	"context"
 
+	"github.com/theopenlane/utils/contextx"
 	google "google.golang.org/api/oauth2/v2"
 )
 
-// unexported key type prevents collisions
-type key int
-
-const (
-	userKey  key = iota
-	errorKey key = iota
+var (
+	userContextKey  = contextx.NewKey[*google.Userinfo]()
+	errorContextKey = contextx.NewKey[error]()
 )
 
 // WithUser returns a copy of ctx that stores the Google Userinfo
 func WithUser(ctx context.Context, user *google.Userinfo) context.Context {
-	return context.WithValue(ctx, userKey, user)
+	return userContextKey.Set(ctx, user)
 }
 
 // UserFromContext returns the Google Userinfo from the ctx
 func UserFromContext(ctx context.Context) (*google.Userinfo, error) {
-	user, ok := ctx.Value(userKey).(*google.Userinfo)
+	user, ok := userContextKey.Get(ctx)
 	if !ok {
 		return nil, ErrContextMissingGoogleUser
 	}
@@ -31,13 +29,13 @@ func UserFromContext(ctx context.Context) (*google.Userinfo, error) {
 
 // WithError returns a copy of context that stores the given error value
 func WithError(ctx context.Context, err error) context.Context {
-	return context.WithValue(ctx, errorKey, err)
+	return errorContextKey.Set(ctx, err)
 }
 
 // ErrorFromContext returns the error value from the ctx or an error that the
 // context was missing an error value
 func ErrorFromContext(ctx context.Context) error {
-	err, ok := ctx.Value(errorKey).(error)
+	err, ok := errorContextKey.Get(ctx)
 	if !ok {
 		return ErrContextMissingErrorValue
 	}
--- providers/oauth2/context.go
+++ providers/oauth2/context.go
@@ -3,27 +3,25 @@ package oauth2
 import (
 	"context"
 
+	"github.com/theopenlane/utils/contextx"
 	"golang.org/x/oauth2"
 )
 
-// unexported key type prevents collisions
-type key int
-
-const (
-	tokenKey    key = iota
-	stateKey    key = iota
-	errorKey    key = iota
-	redirectKey key = iota
+var (
+	tokenContextKey    = contextx.NewKey[*oauth2.Token]()
+	stateContextKey    = contextx.NewKey[string]()
+	errorContextKey    = contextx.NewKey[error]()
+	redirectContextKey = contextx.NewKey[string]()
 )
 
 // WithState returns a copy of ctx that stores the state value
 func WithState(ctx context.Context, state string) context.Context {
-	return context.WithValue(ctx, stateKey, state)
+	return stateContextKey.Set(ctx, state)
 }
 
 // StateFromContext returns the state value from the ctx
 func StateFromContext(ctx context.Context) (string, error) {
-	state, ok := ctx.Value(stateKey).(string)
+	state, ok := stateContextKey.Get(ctx)
 
 	if !ok {
 		return "", ErrContextMissingStateValue
@@ -34,12 +32,12 @@ func StateFromContext(ctx context.Context) (string, error) {
 
 // WithToken returns a copy of ctx that stores the Token
 func WithToken(ctx context.Context, token *oauth2.Token) context.Context {
-	return context.WithValue(ctx, tokenKey, token)
+	return tokenContextKey.Set(ctx, token)
 }
 
 // TokenFromContext returns the Token from the ctx
 func TokenFromContext(ctx context.Context) (*oauth2.Token, error) {
-	token, ok := ctx.Value(tokenKey).(*oauth2.Token)
+	token, ok := tokenContextKey.Get(ctx)
 
 	if !ok {
 		return nil, ErrContextMissingToken
@@ -50,13 +48,13 @@ func TokenFromContext(ctx context.Context) (*oauth2.Token, error) {
 
 // WithError returns a copy of context that stores the given error value
 func WithError(ctx context.Context, err error) context.Context {
-	return context.WithValue(ctx, errorKey, err)
+	return errorContextKey.Set(ctx, err)
 }
 
 // ErrorFromContext returns the error value from the ctx or an error that the
 // context was missing an error value
 func ErrorFromContext(ctx context.Context) error {
-	err, ok := ctx.Value(errorKey).(error)
+	err, ok := errorContextKey.Get(ctx)
 	if !ok {
 		return ErrContextMissingErrorValue
 	}
@@ -66,12 +64,12 @@ func ErrorFromContext(ctx context.Context) error {
 
 // WithRedirectURL returns a copy of ctx that stores the redirect value
 func WithRedirectURL(ctx context.Context, redirect string) context.Context {
-	return context.WithValue(ctx, redirectKey, redirect)
+	return redirectContextKey.Set(ctx, redirect)
 }
 
 // RedirectFromContext returns the redirect value from the ctx
 func RedirectFromContext(ctx context.Context) string {
-	redirect, ok := ctx.Value(redirectKey).(string)
+	redirect, ok := redirectContextKey.Get(ctx)
 
 	if !ok {
 		return ""
